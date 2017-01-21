98c4d8278c0a8b3315ac644f4a28692d06a0bf49
Serene Talia Namini <talia.namini@gmail.com>
2017-01-20 19:53:20 -0800

    yup

--- config/orm.js
+++ config/orm.js
@@ -32,6 +32,7 @@ var orm = {
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
+        console.log("orm.all error: " + err);
       }
       cb(result);
     });
